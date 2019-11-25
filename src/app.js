f51d22cf6b19b0b2cf0fa8ee33206efe958b2f6f
loteresa2 <loteresa2@gmail.com>
2019-11-24 22:20:37 -0500

    Adding in react and react dom. Since babel is not enabled yet, it doesn't recognize jsx.  So we wrote it in react code.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('in app.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'testing23')
+
+ReactDOM.render(template, document.getElementById('app'));
