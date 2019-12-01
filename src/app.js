7534e84a778357f378eae2fd8a406042a6c559ae
loteresa2 <loteresa2@gmail.com>
2019-12-01 17:25:09 -0500

    Adding option modal

--- src/app.js
+++ src/app.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss'
 
-// ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-const Layout = (props) => {
-    return (
-        <div>
-          <p>Header</p>
-          {props.children}
-          <p>Footer</p>
-        </div>
-    );
-};
-
-ReactDOM.render((
-    <Layout>
-      <div>
-        <h1>Page title</h1>
-        <p>This is my page</p>
-      </div>
-    </Layout>
-), document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
